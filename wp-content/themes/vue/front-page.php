b24ec59fd52fbd4346bbaf47bdd220ce357682c2
Jurate-m <dev.jurate@gmail.com>
2023-06-23 18:59:14 +0300

    Vue component for rendering Blog Listing

--- wp-content/themes/vue/front-page.php
+++ wp-content/themes/vue/front-page.php
@@ -10,21 +10,8 @@
  */
 
 get_header();
-?>
 
-<alert message="<?php echo get_post_meta(1, 'message') ?>"></alert>
-
-<?php
-
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
-
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // End of the loop.
+get_template_part('template-parts/content/content-posts');
 
 get_footer();
+
--- wp-content/themes/vue/src/app.js
+++ wp-content/themes/vue/src/app.js
@@ -1,11 +1,21 @@
 import { createApp } from 'vue';
 import Alert from './Alert.vue';
+import BaseCard from "./UI/BaseCard.vue";
+import BlogListing from "./Blog/BlogListing.vue";
+import BlogListingItem from "./Blog/BlogListingItem.vue";
 
 addEventListener("DOMContentLoaded", (event) => {
-    var dom = document.getElementById('content');
+    var dom = document.getElementById("content");
+
     const app = createApp({
         components: {
-            Alert
-        }
-    }).mount(dom);
+            Alert,
+            BlogListing,
+            BlogListingItem,
+        },
+    });
+
+    app.component("base-card", BaseCard);
+
+    app.mount(dom);
 });
