32abd5783a27eddd840d515e365d511a801b7d38
Jurate-m <dev.jurate@gmail.com>
2023-06-29 11:13:34 +0300

    Included jQuery

--- wp-content/themes/vue/functions.php
+++ wp-content/themes/vue/functions.php
@@ -3,6 +3,7 @@
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('vue-script', get_stylesheet_directory_uri() . '/dist/app.js');
     wp_enqueue_style('tailwind', get_stylesheet_directory_uri() . '/dist/style.css');
+    wp_enqueue_script('jQuery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js');
 });
  
 add_filter('acf/settings/save_json', function( $path ) {
