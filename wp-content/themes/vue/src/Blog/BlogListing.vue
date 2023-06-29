37b8bb553ce3ddd903acb6e7932d1f7cb8bf2406
Jurate-m <dev.jurate@gmail.com>
2023-06-29 11:13:01 +0300

    Vue components

--- wp-content/themes/vue/src/Blog/BlogListing.vue
+++ wp-content/themes/vue/src/Blog/BlogListing.vue
@@ -1,22 +1,52 @@
 <template>
-    <div v-for="post in posts">
-        <blog-listing-item :title="post.name" :content="post.excerpt"></blog-listing-item>
+    <blog-categories :categories="categories" @setCategory="setActiveCat"></blog-categories>
+
+    <div class="flex flex-wrap">
+        <blog-listing-item class="basis-1/3" 
+            v-for="post in filteredPosts"
+            :title="post.name" 
+            :content="post.excerpt" 
+            key="{{ post.id }}" 
+            >
+        </blog-listing-item>
     </div>
 </template>
 
 <script>
 import BlogListingItem from './BlogListingItem.vue';
+import BlogCategories from './BlogCategories.vue';
 
     export default {
+        props: [ 
+            'posts', 
+            'categories' 
+        ],
+
+        data() {
+            return {
+                currentCategoryId: 0
+            }
+        },
 
         components: {
-            BlogListingItem
+            BlogListingItem,
+            BlogCategories
         },
 
-        mounted() {
-            console.log(this.posts);
+        computed: {
+
+            filteredPosts() {
+                if ( this.currentCategoryId == 0 ) return this.posts;
+
+                return this.posts.filter(post => post.categories.map(category => category.id).includes(this.currentCategoryId));
+            }
         },
 
-        props: [ 'posts' ]
+        methods: {
+            setActiveCat(id) {
+                this.currentCategoryId = id;
+            }
+        }
+
     }
 </script>
