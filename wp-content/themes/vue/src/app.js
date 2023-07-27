3e6f30ecc983229a2c37b931026cfd7b27585034
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-07-27 16:16:57 +0100

    Adds change, need to npm link the main library though.

--- wp-content/themes/vue/src/app.js
+++ wp-content/themes/vue/src/app.js
@@ -4,6 +4,7 @@ import BaseCard from "./UI/BaseCard.vue";
 import BlogListing from "./Blog/BlogListing.vue";
 import BlogListingItem from "./Blog/BlogListingItem.vue";
 import BlogCategories from "./Blog/BlogCategories.vue";
+import { Teaser } from 'vue3-customised-component-library';
 
 addEventListener("DOMContentLoaded", (event) => {
     var dom = document.getElementById("content");
@@ -14,6 +15,7 @@ addEventListener("DOMContentLoaded", (event) => {
             BlogListing,
             BlogListingItem,
             BlogCategories,
+            Teaser,
         },
     });
 
